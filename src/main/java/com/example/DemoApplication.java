9f68d7aaf7abd89e3b6d35e2f12986dfdf5c2761
Dmytro Proskurnia <dmytro.proskurnia@gmail.com>
2017-03-09 20:20:07 +0200

    upd

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,11 +2,91 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @SpringBootApplication
-public class DemoApplication {
+@Configuration
+@EnableWebMvc
+public class DemoApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("classpath:resources/");
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public ServletContextTemplateResolver templateResolver() {
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+        templateResolver.setTemplateMode("HTML5");
+        templateResolver.setPrefix("classpath:templates/");
+        templateResolver.setSuffix(".html");
+        // templateResolver.setCacheTTLMs(3600000L);
+        templateResolver.setCacheable(Boolean.FALSE);
+        return templateResolver;
+    }
+
+    @Bean
+    public TemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateEngineMessageSource(getMessageSource());
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine((SpringTemplateEngine) templateEngine());
+        viewResolver.getTemplateEngine().setTemplateEngineMessageSource(getMessageSource());
+        viewResolver.setCharacterEncoding("UTF-8");
+        viewResolver.setOrder(1);
+        return viewResolver;
+    }
+
+    @Bean(name = "messageSource")
+    public MessageSource getMessageSource() {
+        ReloadableResourceBundleMessageSource resource = new ReloadableResourceBundleMessageSource();
+        resource.setUseCodeAsDefaultMessage(true);
+        resource.setBasename("classpath:locale/messages");
+        resource.setDefaultEncoding("UTF-8");
+        return resource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new CookieLocaleResolver();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
--- src/main/java/com/example/controllers/TenantsController.java
+++ src/main/java/com/example/controllers/TenantsController.java
@@ -1,20 +1,32 @@
 package com.example.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.TemplateEngine;
+
+import java.util.Locale;
 
 /**
  * Created by dmpr0116 on 07.03.2017.
  */
 
 @Controller
-//@RequestMapping("/tenants")
-@RequestMapping("/")
+@RequestMapping("/tenants")
 public class TenantsController {
 
+    @Autowired
+    MessageSource messageSource;
+
+    @Autowired
+    TemplateEngine templateEngine;
+
     @RequestMapping
-    public String allCustomers(){
+    public String allCustomers(Model model) {
+        String s = messageSource.getMessage("all.tenants", null, Locale.getDefault());
+        model.addAttribute("list", new int[]{1, 2, 3, 4});
         return "tenants/all";
     }
 }
